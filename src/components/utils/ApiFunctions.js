28bb78341db1ec52559e52a5dd08694920cce67f
TripleNgg <153419080+TripleNgg@users.noreply.github.com>
2024-03-11 23:15:36 +0700

    change port to access to backend

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:9192"
+    baseURL: "https://hotel-project-3mft.onrender.com"
 });
 
 export const getHeader = ()=> {
